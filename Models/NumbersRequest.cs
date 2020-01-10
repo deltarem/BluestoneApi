072f4725a4108383b76358c2377e6b483f64c4a4
Dogan UGUR <dogan.ugur@ihsmarkit.com>
2020-01-10 20:24:01 +0000

    code cleaning

--- Models/NumbersRequest.cs
+++ Models/NumbersRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BluestoneApi.Models
+﻿namespace BluestoneApi.Models
 {
     public class NumbersRequest
     {
